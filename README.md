772ca00ff447271b7a37b6b83323b3e4cfa567da
DiogoOliveiraReis <diogo.reis@fe.up.pt>
2022-04-01 11:10:04 +0100

    README update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FEUP-EDAA
+
+## How to see the graph
+python networkx/view.py
--- networkx/view.py
+++ networkx/view.py
@@ -2,7 +2,6 @@ from xml.dom.minicompat import NodeList
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
-import re
 
 G = nx.MultiGraph()
 
