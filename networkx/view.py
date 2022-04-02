39d197512004e2a8dfd8fcd6e57939f0674c9600
DiogoOliveiraReis <diogo.reis@fe.up.pt>
2022-04-02 18:05:00 +0100

    fix: graph visualization now supports roads

--- networkx/view.py
+++ networkx/view.py
@@ -1,15 +1,14 @@
-from xml.dom.minicompat import NodeList
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 
 G = nx.MultiGraph()
 
-graph_data_nodes = np.loadtxt('exports/nodes.csv', dtype='str', delimiter=',', encoding="utf-8-sig")
-graph_data_edges = np.loadtxt('exports/edges.csv', dtype='str', delimiter=',', encoding="utf-8-sig")
+graph_data_nodes = np.loadtxt('data/reuse/nodes.csv', dtype='str', delimiter=',', encoding="utf-8-sig")
+graph_data_edges = np.loadtxt('data/reuse/edges.csv', dtype='str', delimiter=',', encoding="utf-8-sig")
 
 for node in graph_data_nodes:
-        G.add_node(node[5],pos=(float(node[0]), float(node[1])))
+        G.add_node(node[4],pos=(float(node[0]), float(node[1])))
 
 G.add_edges_from(graph_data_edges)
 
@@ -19,30 +18,54 @@ busNodes = []
 metroNodes = []
 busEdges = []
 metroEdges = []
+walkNodes = []
+walkEdges = []
 
 for node in G.nodes:
     if 'bus' in node:
         busNodes.append(node)
     if 'metro' in node:
         metroNodes.append(node)
+    if 'walk' in node:
+        walkNodes.append(node)
 
 for node in G.edges:
-    if 'bus' in node[0]:
+    if ('bus' in node[0] and 'bus' in node[1]):
         busEdges.append(node)
-    if 'metro' in node[1]:
+    if ('metro' in node[0] and 'metro' in node[1]):
         metroEdges.append(node)
+    if ('walk' in node[0] and 'walk' in node[1]):
+        walkEdges.append(node)
 
 # Draw Bus Nodes
-nx.draw_networkx_nodes(G, pos, nodelist=busNodes, node_size=25, node_color='tab:blue', alpha=0.9)
+try: nx.draw_networkx_nodes(G, pos, nodelist=busNodes, node_size=25, node_color='tab:blue', alpha=0.9)
+except nx.NetworkXError:
+    print('Error drawing bus nodes')
 
 # Draw Metro Nodes
-nx.draw_networkx_nodes(G, pos, nodelist=metroNodes, node_size=25, node_color='tab:red', alpha=0.9)
+try: nx.draw_networkx_nodes(G, pos, nodelist=metroNodes, node_size=25, node_color='tab:red', alpha=0.9)
+except nx.NetworkXError:
+    print('Error drawing metro nodes')
+
+# Draw Walk Nodes
+try: nx.draw_networkx_nodes(G, pos, nodelist=walkNodes, node_size=25, node_color='tab:green', alpha=0.9)
+except nx.NetworkXError:
+    print('Error drawing walk nodes')
 
 # Draw Bus Edges
-nx.draw_networkx_edges(G, pos, edgelist=busEdges, edge_color='tab:blue')
+try: nx.draw_networkx_edges(G, pos, edgelist=busEdges, edge_color='tab:blue')
+except nx.NetworkXError:
+    print('Error drawing bus edges')
 
 # Draw Metro Edges
-nx.draw_networkx_edges(G, pos, edgelist=metroEdges, edge_color='tab:red')
+try: nx.draw_networkx_edges(G, pos, edgelist=metroEdges, edge_color='tab:red')
+except nx.NetworkXError:
+    print('Error drawing metro edges')
+
+# Draw Walk Edges
+try: nx.draw_networkx_edges(G, pos, edgelist=walkEdges, edge_color='tab:green')
+except nx.NetworkXError:
+    print('Error drawing walk edges')
 
 # Draw Labels
 #nx.draw_networkx_labels(G, pos, font_size=10)
